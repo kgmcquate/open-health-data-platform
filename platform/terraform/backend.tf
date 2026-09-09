7698e1c1802d01d7f1a89c07462dfc471ccc5328
kevinmcquate <kgmcquate@gmail.com>
2026-09-09 14:54:29 -0400

    fixed backend

--- platform/terraform/backend.tf
+++ platform/terraform/backend.tf
@@ -36,9 +36,9 @@ terraform {
 
     # use_lockfile = true
     skip_credentials_validation = true
-    skip_metadata_api_check = true
-    skip_region_validation = true
-    skip_requesting_account_id = true
-    skip_s3_checksum = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
