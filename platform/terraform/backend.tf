229538920749764cc62eac46465325e53a8313c0
kevinmcquate <kgmcquate@gmail.com>
2026-09-09 14:53:50 -0400

    fixed backend

--- platform/terraform/backend.tf
+++ platform/terraform/backend.tf
@@ -26,5 +26,19 @@
 #
 # The deploy-infra workflow passes all of this from secrets.
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "terraform-state"
+    key    = "platform/terraform.tfstate"
+    region = "auto"
+    endpoints = {
+      s3 = "https://32dcca0e1334dec31eaa52a63f4b05b9.r2.cloudflarestorage.com"
+    }
+
+    # use_lockfile = true
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    skip_region_validation = true
+    skip_requesting_account_id = true
+    skip_s3_checksum = true
+  }
 }
